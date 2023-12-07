de117a3be07d79d8351c482314f5d24a772a82eb
KASSY <133909539+DollyWhite06@users.noreply.github.com>
2023-12-07 14:32:12 -0600

    Actualizar Kassyta
    blade corre

---
+++ Kassyta/agradecimiento.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agradecimiento</title>
+</head>
+<body style="text-align: center; font-family: Arial, sans-serif;">
+
+    <h1 style="color: #000; font-size: 24px; font-weight: bold;">¡Gracias por verificar tu correo!</h1>
+    
+    <p style="color: #333; font-size: 16px;">Equipo PewSecure</p>
+
+</body>
+</html>
