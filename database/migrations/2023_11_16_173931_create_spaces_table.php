0b9a8a747be53ba26da668007c5a03aa10db6ec6
Darío Ramos <133318606+DarioRamosTec@users.noreply.github.com>
2023-12-07 08:46:15 -0600

    Change the nullable condition of name and description on spaces.

--- database/migrations/2023_11_16_173931_create_spaces_table.php
+++ database/migrations/2023_11_16_173931_create_spaces_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('spaces', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->nullable();
-            $table->string('description')->nullable();
+            $table->string('name');
+            $table->string('description');
             $table->foreignId('user');
             $table->timestamps();
         });
